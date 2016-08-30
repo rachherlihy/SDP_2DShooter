f7888eae41c997196bd9c81c855c508864884d84
rachherlihy <rach.herlihy@gmail.com>
2016-08-30 19:34:18 +1200

    Add player
    Started adding player and player movement

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+    public float speed;
+    
+    void FixedUpdate()
+    {
+        // set mouse pos to world space
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePos);
+        // gives direction for where mouse is looking
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePos, Vector3.forward);
+        transform.rotation = rot;
+        transform.eurlerAngles = new Vector3(0, 0, transform.eurlerAngles.z);
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+    }
+}
