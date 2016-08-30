1cf096ac2bf77175248a64f6626d33a4c0d530b9
JK1010 <minsookimnz@gmail.com>
2016-08-30 12:36:04 +1200

    Jack(Start and End scene)
    Start and End scene

---
+++ Assets/Scripts/StartingScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartingScene : MonoBehaviour
+{
+    public Transform instructionMenu;
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+    public void QuitGame()
+    {
+        if(Input.GetKey("escape"))
+        Application.Quit();
+    
+}
+
+    public void InstructionMenu(bool click)
+    {
+        if (click == true)
+        {
+            instructionMenu.gameObject.SetActive(click);
+        }
+
+
+    }
+}
