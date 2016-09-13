7e886439b20a3a75e5ad4057beb4d1644b5603e3
Jayce-C <Blackswankk@gmail.com>
2016-09-14 01:14:14 +1200

    shoot bullets,enemy HP,Kill enemy

---
+++ Assets/Scripts/BulletMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMove : MonoBehaviour {
+	public Vector2 speed = new Vector2 (50, 50);
+
+	public Vector2 direction = new Vector2 (-1, 0);
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3 (speed.x * direction.x, speed.y * direction.y, 0);
+
+		movement *= Time.deltaTime;
+
+		transform.Translate (movement);
+	}
+}
